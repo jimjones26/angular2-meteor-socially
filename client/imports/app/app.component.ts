b2bfa2cbd34fbbd2a4d2950859f4e45f280c049e
jimjones26 <jjones@Jamess-MacBook-Pro.local>
2016-12-11 18:10:15 -0500

    refactor parties list to seperate component

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-
-import { Party } from './../../../both/models/party.model';
-import { Parties } from './../../../both/collections/parties.collection';
 
 import template from './app.component.html';
 
@@ -10,14 +6,4 @@ import template from './app.component.html';
   selector: 'app-root',
   template
 })
-export class AppComponent {
-  parties: Observable<Party[]>;
-
-  constructor() {
-    this.parties = Parties.find({}).zone();
-  }
-
-  removeParty(party: Party): void {
-    Parties.remove(party._id);
-  }
-}
+export class AppComponent { }
